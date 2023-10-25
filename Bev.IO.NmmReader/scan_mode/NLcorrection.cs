d33144da3f22f12d4bd0c66bafa010e7d25a8291
Michael Matus <michael@matus.at>
2023-10-25 23:30:33 +0200

    Automate nonlinearity correction
    The 4tho order correction value is deduced from the circle deformation. The used factor is completly empirical and needs more work.

--- Bev.IO.NmmReader/scan_mode/NLcorrection.cs
+++ Bev.IO.NmmReader/scan_mode/NLcorrection.cs
@@ -1,49 +1,39 @@
-﻿using System;
-using System.IO;
-
-namespace Bev.IO.NmmReader.scan_mode
+﻿namespace Bev.IO.NmmReader.scan_mode
 {
     public class NLcorrection
     {
-        private const double daiCorrection = 0.5e-9;
+        private const double defaultDaiCorrection = 0.5e-9;
 
         public CorrectionStatus Status { get; private set; } = CorrectionStatus.Unknown;
         public double[] CorrectedData { get; }
         public double CorrectionSpan { get; private set; } = 0.0;
 
+        // The Dai correction is performed in any case with the provided correction value
         public NLcorrection(double[] rawData, double[] sinValues, double[] cosValues, double empiricalCorrection)
         {
             NLcorrectionHeydemann heydemann = new NLcorrectionHeydemann(rawData, sinValues, cosValues);
-
-            DebugLog(sinValues, cosValues, heydemann.CorrectedSinValues, heydemann.CorrectedCosValues);
-
             NLcorrectionDai gaoliang = new NLcorrectionDai(heydemann.CorrectedData, heydemann.CorrectedSinValues, heydemann.CorrectedCosValues, empiricalCorrection);
-            //NLcorrectionDai gaoliang = new NLcorrectionDai(heydemann.CorrectedData, sinValues, cosValues, empiricalCorrection);
             CorrectedData = gaoliang.CorrectedData;
             CorrectionSpan = heydemann.CorrectionSpan + gaoliang.CorrectionSpan;
             Status = heydemann.Status;
         }
 
-        public NLcorrection(double[] rawData, double[] sinValues, double[] cosValues) : this(rawData, sinValues, cosValues, daiCorrection) { }
-
-        private void DebugLog(double[] sin, double[] cos, double[] sinCor, double[] cosCor)
+        // The Dai correction is performed with an estimated value or wit the default value, depending on the Heydemann success
+        public NLcorrection(double[] rawData, double[] sinValues, double[] cosValues)
         {
-            using (StreamWriter writer = new StreamWriter("quadrature.csv"))
+            NLcorrectionHeydemann heydemann = new NLcorrectionHeydemann(rawData, sinValues, cosValues);
+            NLcorrectionDai gaoliang;
+            if (heydemann.Status==CorrectionStatus.Corrected)
             {
-                for (int i = 0; i < sin.Length; i += 97)
-                {
-                    double s = sin[i];
-                    double c = cos[i];
-                    double sc = sinCor[i];
-                    double cc = cosCor[i];
-                    writer.WriteLine($"{s}, {c}, {PhiDeg(s,c)}, {Radius(s,c)}, {sc}, {cc}, {PhiDeg(sc, cc)}, {Radius(sc, cc)}");
-                }
+                gaoliang = new NLcorrectionDai(heydemann.CorrectedData, heydemann.CorrectedSinValues, heydemann.CorrectedCosValues);
             }
+            else
+            {
+                gaoliang = new NLcorrectionDai(heydemann.CorrectedData, heydemann.CorrectedSinValues, heydemann.CorrectedCosValues, defaultDaiCorrection);
+            }
+            CorrectedData = gaoliang.CorrectedData;
+            CorrectionSpan = heydemann.CorrectionSpan + gaoliang.CorrectionSpan;
+            Status = heydemann.Status;
         }
-
-        private double Radius(double x, double y) => Math.Sqrt(x * x + y * y);
-        
-        private double PhiDeg(double x, double y) => Math.Atan2(y, x)* 180/Math.PI;
-
     }
 }
--- Bev.IO.NmmReader/scan_mode/NLcorrectionDai.cs
+++ Bev.IO.NmmReader/scan_mode/NLcorrectionDai.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using At.Matus.StatisticPod;
 
 namespace Bev.IO.NmmReader.scan_mode
 {
     public class NLcorrectionDai
     {
+        private const double empiricalNLfactor = 305.25e-10; // in m. This is an empirical constant, found to be usefull for 0.5 nm correction
 
         public CorrectionStatus Status { get; private set; } = CorrectionStatus.Unknown;
         public double CorrectionAmplitude { get; private set; }
         public double CorrectionSpan => CorrectionAmplitude * 2;
         public double[] CorrectedData { get; private set; }
+
+        public NLcorrectionDai(double[] rawData, double[] sinValues, double[] cosValues)
+        {
+            CorrectionAmplitude = EstimateCorrectionAmplitude(sinValues, cosValues);
+            PerformCorrection(rawData, sinValues, cosValues);
+        }
 
         public NLcorrectionDai(double[] rawData, double[] sinValues, double[] cosValues, double empiricalCorrection)
         {
@@ -38,13 +46,61 @@ namespace Bev.IO.NmmReader.scan_mode
             }
         }
 
-        private double Phi(double sin, double cos) => Math.Atan2(cos, sin);
+        private double Phi(double x, double y) => Math.Atan2(y, x);
 
         private double GetCorrection(double sin, double cos) => - CorrectionAmplitude * Math.Sin(4 * Phi(sin, cos));
 
+        private double EstimateCircleDeformation(double[] sinValues, double[] cosValues)
+        {
+            StatisticPod allRadii = new StatisticPod();
+            StatisticPod axisRadii = new StatisticPod();
+            StatisticPod medianRadii= new StatisticPod();
+            for (int i = 0; i < sinValues.Length; i++)
+            {
+                double s = sinValues[i];
+                double c = cosValues[i];
+                double r = Radius(s, c);
+                double phi = PhiDeg(s, c);
+                allRadii.Update(r);
+                if (IsAxis(phi)) axisRadii.Update(r);
+                if (IsMedian(phi)) medianRadii.Update(r);
+            }
+            return (medianRadii.AverageValue - axisRadii.AverageValue) / allRadii.AverageValue;
+        }
+
+        private double EstimateCorrectionAmplitude(double[] sinValues, double[] cosValues)
+        {
+            return empiricalNLfactor * EstimateCircleDeformation(sinValues, cosValues);
+        }
 
         private double Radius(double x, double y) => Math.Sqrt(x * x + y * y);
 
         private double PhiDeg(double x, double y) => Phi(x, y) * 180 / Math.PI;
+
+        private bool IsAxis(double phi)
+        {
+            if (IsNear(phi, 0)) return true;
+            if (IsNear(phi, 90)) return true;
+            if (IsNear(phi, 180)) return true;
+            if (IsNear(phi, -90)) return true;
+            if (IsNear(phi, -180)) return true;
+            return false;
+        }
+
+        private bool IsMedian(double phi)
+        {
+            if (IsNear(phi, 45)) return true;
+            if (IsNear(phi, 135)) return true;
+            if (IsNear(phi, -45)) return true;
+            if (IsNear(phi, -135)) return true;
+            return false;
+        }
+
+        private bool IsNear(double phi, double target)
+        {
+            const double eps = 2;
+            if (Math.Abs(target - phi) < eps) return true;
+            return false;
+        }
     }
 }
