e19982fafc67c1f252f6c8b712f60dba1caf36a8
Michael Matus <michael@matus.at>
2025-03-15 17:57:43 +0100

    Refactor NmmFileName

--- Bev.IO.NmmReader/NmmFileName.cs
+++ Bev.IO.NmmReader/NmmFileName.cs
@@ -11,12 +11,11 @@ namespace Bev.IO.NmmReader
         public NmmFileName(string path)
         {
             BasePath = Path.ChangeExtension(path, null);
-            ScanIndex = 0;
         }
 
         public string BaseFileName => Path.GetFileNameWithoutExtension(BasePath);
-        public string BasePath { get; private set; }
-        public int ScanIndex { get; private set; }
+        public string BasePath { get; }
+        public int ScanIndex { get; private set; } = 0;
 
         // setting index to 0 is used for a single scan without special filenames
         public void SetScanIndex(int index)
@@ -25,69 +24,32 @@ namespace Bev.IO.NmmReader
             ScanIndex = index;
         }
 
-        public string GetDatFileNameForScanIndex(ScanDirection direction)
-        {
-            return GetFileName(direction, "dat", ScanIndex);
-        }
+        public string GetDatFileNameForScanIndex(ScanDirection direction) => GetFileName(direction, "dat", ScanIndex);
 
-        public string GetPosFileNameForScanIndex(ScanDirection direction)
-        {
-            return GetFileName(direction, "pos", ScanIndex);
-        }
+        public string GetPosFileNameForScanIndex(ScanDirection direction) => GetFileName(direction, "pos", ScanIndex);
 
-        public string GetIndFileNameForScanIndex(ScanDirection direction)
-        {
-            return GetFileName(direction, "ind", ScanIndex);
-        }
+        public string GetIndFileNameForScanIndex(ScanDirection direction) => GetFileName(direction, "ind", ScanIndex);
 
-        public string GetDscFileNameForScanIndex(ScanDirection direction)
-        {
-            return GetFileName(direction, "dsc", ScanIndex);
-        }
+        public string GetDscFileNameForScanIndex(ScanDirection direction) => GetFileName(direction, "dsc", ScanIndex);
 
         // overloads without parameters return the forward scan files
-        public string GetDatFileName()
-        {
-            return GetFreeFileName("dat");
-        }
+        public string GetDatFileName() => GetFreeFileName("dat");
 
-        public string GetPosFileName()
-        {
-            return GetFreeFileName("pos");
-        }
+        public string GetPosFileName() => GetFreeFileName("pos");
 
-        public string GetIndFileName()
-        {
-            return GetFreeFileName("ind");
-        }
+        public string GetIndFileName() => GetFreeFileName("ind");
 
-        public string GetDscFileName()
-        {
-            return GetFreeFileName("dsc"); ;
-        }
+        public string GetDscFileName() => GetFreeFileName("dsc");
 
         // 3D-files have neither a direction nor multiple scans
-        public string Get3DFileName()
-        {
-            return GetFreeFileName("3d");
-        }
+        public string Get3DFileName() => GetFreeFileName("3d");
 
-        public string GetTrgFileName()
-        {
-            return GetFreeFileName("trg");
-        }
+        public string GetTrgFileName() => GetFreeFileName("trg");
 
         // this may be used to generate names for output files.
-        public string GetFreeFileName(string extension)
-        {
-            return GetFileName(ScanDirection.Forward, extension, 0);
-        }
+        public string GetFreeFileName(string extension) => GetFileName(ScanDirection.Forward, extension, 0);
 
-        public string GetFreeFileNameWithIndex(string extension)
-        {
-            return GetFileName(ScanDirection.Forward, extension, ScanIndex);
-        }
-
+        public string GetFreeFileNameWithIndex(string extension) => GetFileName(ScanDirection.Forward, extension, ScanIndex);
 
         private string GetFileName(ScanDirection direction, string extension, int index)
         {
@@ -106,10 +68,7 @@ namespace Bev.IO.NmmReader
         {
             if (index > 0)
             {
-                return Path.ChangeExtension(
-                    BasePath + string.Format("_{0}", index),
-                    extension
-                );
+                return Path.ChangeExtension(BasePath + $"_{index}", extension);
             }
             return Path.ChangeExtension(BasePath, extension);
         }
@@ -118,10 +77,7 @@ namespace Bev.IO.NmmReader
         {
             if (index > 0)
             {
-                return Path.ChangeExtension(
-                    BasePath + string.Format("b_{0}", index),
-                    extension
-                );
+                return Path.ChangeExtension(BasePath + $"b_{index}", extension);
             }
             return Path.ChangeExtension(BasePath + "b", extension);
         }
@@ -132,6 +88,5 @@ namespace Bev.IO.NmmReader
                 return $"[NmmFileName: {BaseFileName}]";
             return $"[NmmFileName: {BaseFileName} for scan: {ScanIndex}]";
         }
-
     }
 }
